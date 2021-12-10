3398002a5fce387166d7f041dd68a49f8393e8b8
AchRef <78941591+AchRef864@users.noreply.github.com>
2021-12-10 21:56:51 +0100

    Add files via upload

---
+++ PopCat.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Edge("D:\Education\edgedriver_win64\msedgedriver.exe")
+
+def give_url(link):
+    driver.get(link)
+
+def one_click(cat):
+    WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.ID, cat)))
+    driver.find_element_by_id(cat).click()
+
+give_url("https://popcat.click/")
+
+while True:
+    one_click("app")
